ce2c9ba9b858cd3e7a2cb9887f9c77d10d163ae4
fordcer <76239586+fordcer@users.noreply.github.com>
2022-05-10 21:15:31 +0700

    50% work

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'elephant': "url('../src/asset/bg-elep.jpg')"
+      }
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
