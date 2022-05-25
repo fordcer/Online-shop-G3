299518347a5d45a7ddee1f98cb6a41decc5bd50c
fordcer <76239586+fordcer@users.noreply.github.com>
2022-05-25 14:49:16 +0700

    fixed styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,18 +6,6 @@ module.exports = {
         'elephant': "url('../src/asset/bg-elep.jpg')"
       }
     },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
-    },
   },
   plugins: [],
 }
